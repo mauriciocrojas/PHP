a40822f5b1f8110cc0da0412f9c86f416fc8eefa
mauriciocrojas <mauriciocc.rojas@gmail.com>
2022-08-28 15:46:55 -0300

    Ejs

--- Clase01/Ejercicio10.php
+++ Clase01/Ejercicio10.php
@@ -8,8 +8,17 @@
 // Mauricio Rojas
 // Ej 10
 
+$indice = 1;
 
+$lapicera = array(
+    $lapicera[0] = array("Color" => "Azul", "Marca" => "Bic", "Trazo" => "Fino", "Precio" => 100),
+    $lapicera[1] = array("Color" => "Verde", "Marca" => "Parker", "Trazo" => "Grueso", "Precio" => 150),
+    $lapicera[2] = array("Color" => "Negra", "Marca" => "Unix", "Trazo" => "Normal", "Precio" => 200)  
+);
 
-
+foreach($lapicera as $lap){
+    echo "<b> Lapicera ". $indice++. ":</b><br>";
+    echo "Color: ".$lap["Color"]. ", ". "Marca: ". $lap["Marca"]. ", ". "Trazo: ". $lap["Trazo"]. ", ". "Precio: ". $lap["Precio"]."<br><br>";
+}
 
 ?>
