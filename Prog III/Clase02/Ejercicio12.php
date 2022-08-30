1fbc3fb4f9c0e08962ea0c73969116a06c24cb19
mauriciocrojas <mauriciocc.rojas@gmail.com>
2022-08-30 20:57:09 -0300

    Clase02

---
+++ Prog III/Clase02/Ejercicio12.php
@@ -0,0 +1,25 @@
+<?php
+/*
+Aplicación No 12 (Invertir palabra)
+Realizar el desarrollo de una función que reciba un Array de caracteres y que invierta el orden
+de las letras del Array.
+Ejemplo: Se recibe la palabra “HOLA” y luego queda “ALOH”.
+
+Rojas Mauricio
+Ej 12
+*/
+
+$palabra = array('H','O','L','A');
+
+
+function invertirChar($palabra){
+    for($i = count($palabra) - 1 ; $i >= 0; $i--){
+         
+            echo $palabra[$i];
+        
+    }
+}
+
+invertirChar($palabra);
+
+?>
---
+++ Prog III/Clase02/Ejercicio13.php
@@ -0,0 +1,43 @@
+<?php
+
+/*
+Aplicación No 13 (Invertir palabra)
+Crear una función que reciba como parámetro un string ($palabra) y un entero ($max). La
+función validará que la cantidad de caracteres que tiene $palabra no supere a $max y además
+deberá determinar si ese valor se encuentra dentro del siguiente listado de palabras válidas:
+“Recuperatorio”, “Parcial” y “Programacion”. Los valores de retorno serán:
+1 si la palabra pertenece a algún elemento del listado.
+0 en caso contrario.
+
+Rojas Mauricio
+Ej 13
+*/
+
+$palabra = "Parcial";
+$max = 8;
+
+function tope ($palabra, $max){
+    
+    $rta = "";
+
+    if(strlen($palabra) < $max){
+
+        $lista = array ("Recuperatorio", "Parcial", "Programacion");
+
+        foreach($lista as $dato){
+            if($palabra == $dato){
+                $rta = "1. La palabra se encuentra en el listado";
+                return $rta; 
+                break;
+            }else{
+                $rta = "0. La palabra no se encuentra en el listado";
+            }
+        }
+
+    }else {
+        $rta = "La palabra es mayor al máximo permitido";;
+    }
+    return $rta;
+}
+
+echo tope($palabra, $max);
